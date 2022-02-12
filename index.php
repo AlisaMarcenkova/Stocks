8916494e48e74a1a4bd61cfd2add1bb692108690
Alisa <djalise@gmail.com>
2022-02-12 23:30:06 +0200

    Update

--- index.php
+++ index.php
@@ -8,38 +8,40 @@ $client = new Finnhub\Api\DefaultApi(
     $config
 );
 
-$search = $client->companyProfile2($_GET['search'] ?? '');
+$search = strtoupper($_GET['search'] ?? '');
 
 ?>
 <link rel="stylesheet" href="style.css">
-<form method="get" action="/">
-    <input name="search" placeholder="search" value="">
-    <button type="submit">Search</button>
-    <?php if ($search === $client->companyProfile2("$search")): ?>
-        <?php echo $client->companyProfile2("$search")->getTicker() . " " . $client->quote("$search")->getC() . "<br>";
-        if($client->quote("$search")->getDp() < 0){
-            echo $client->quote("$search")->getDp() . "% (" . $client->quote("$search")->getD() . ")" . "&#x1F53B" . "<br>";
-        }else{
-            $client->quote("$search")->getDp() . "% (" . $client->quote("$search")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
-        }
-        ?>
-    <?php endif; ?>
-</form>
+<div class="container">
+    <form method="get" action="/">
+        <input name="search" placeholder="search" value="">
+        <button type="submit">Search</button>
+        <?php if ($search === $client->companyProfile2("$search")): ?>
+            <?php echo $client->companyProfile2("$search")->getTicker() . " " . $client->quote("$search")->getC() . "<br>";
+            if ($client->quote("$search")->getDp() < 0) {
+                echo $client->quote("$search")->getDp() . "% (" . $client->quote("$search")->getD() . ")" . "&#x1F53B" . "<br>";
+            } else {
+                $client->quote("$search")->getDp() . "% (" . $client->quote("$search")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
+            }
+            ?>
+        <?php endif; ?>
+    </form>
+</div>
 <div class="container">
     <div class="containerOne">
         <?php echo $client->companyProfile2("AAPL")->getTicker() . " " . $client->quote("AAPL")->getC() . "<br>";
-            if($client->quote("AAPL")->getDp() < 0){
-                echo $client->quote("AAPL")->getDp() . "% (" . $client->quote("AAPL")->getD() . ")" . "&#x1F53B" . "<br>";
-            }else{
-                $client->quote("AAPL")->getDp() . "% (" . $client->quote("AAPL")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
-            }
+        if ($client->quote("AAPL")->getDp() < 0) {
+            echo $client->quote("AAPL")->getDp() . "% (" . $client->quote("AAPL")->getD() . ")" . "&#x1F53B" . "<br>";
+        } else {
+            $client->quote("AAPL")->getDp() . "% (" . $client->quote("AAPL")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
+        }
         ?>
     </div>
     <div class="containerTwo">
         <?php echo $client->companyProfile2("AMZN")->getTicker() . " " . $client->quote("AMZN")->getC() . "<br>";
-        if($client->quote("AMZN")->getDp() < 0){
+        if ($client->quote("AMZN")->getDp() < 0) {
             echo $client->quote("AMZN")->getDp() . "% (" . $client->quote("AMZN")->getD() . ")" . "&#x1F53B" . "<br>";
-        }else{
+        } else {
             $client->quote("AMZN")->getDp() . "% (" . $client->quote("AMZN")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
         }
         ?>
@@ -48,18 +50,18 @@ $search = $client->companyProfile2($_GET['search'] ?? '');
 <div class="container">
     <div class="containerThree">
         <?php echo $client->companyProfile2("MSFT")->getTicker() . " " . $client->quote("MSFT")->getC() . "<br>";
-        if($client->quote("MSFT")->getDp() < 0){
+        if ($client->quote("MSFT")->getDp() < 0) {
             echo $client->quote("MSFT")->getDp() . "% (" . $client->quote("MSFT")->getD() . ")" . "&#x1F53B" . "<br>";
-        }else{
+        } else {
             $client->quote("MSFT")->getDp() . "% (" . $client->quote("MSFT")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
         }
         ?>
     </div>
     <div class="containerFour">
         <?php echo $client->companyProfile2("XOM")->getTicker() . " " . $client->quote("XOM")->getC() . "<br>";
-        if($client->quote("XOM")->getDp() > 0){
+        if ($client->quote("XOM")->getDp() > 0) {
             echo $client->quote("XOM")->getDp() . "% (" . $client->quote("XOM")->getD() . ")" . "<span style='color: green;'> &#x25B2</span>" . "<br>";
-        }else{
+        } else {
             $client->quote("XOM")->getDp() . "% (" . $client->quote("XOM")->getD() . ")" . "&#x1F53B" . "<br>";
         }
         ?>
